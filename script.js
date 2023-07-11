70094dee145a5d0dca20d3f2597fc49e8d38ec45
Anne <steamengine.a@gmail.com>
2023-07-10 22:59:09 -0400

    Add comments

--- script.js
+++ script.js
@@ -7,10 +7,14 @@ document.querySelector('.number').textContent = secretNumber;
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
+
+  // When there is no input
   if (!guess) {
     document.querySelector('.message').textContent = '⛔️ No Number!';
+    // When player wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉 Correct Number!';
+    // When guess is too high
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '📈 Too high!';
@@ -20,6 +24,7 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.message').textContent = '💥 You lost the game!';
       document.querySelector('.score').textContent = 0;
     }
+    // When guess is too low
   } else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '📉 Too low!';

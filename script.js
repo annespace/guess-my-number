266698612d945eafcec2d288454d62c78a916737
Anne <steamengine.a@gmail.com>
2023-07-10 21:35:06 -0400

    Update if logic

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const secretNumber = Math.trunc(Math.random() * 10) + 1;
+document.querySelector('.number').textContent = secretNumber;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔️ No Number!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉 Correct Number!';
+  } else if (guess > secretNumber) {
+    document.querySelector('.message').textContent = '📈 Too high!';
+  } else if (guess < secretNumber) {
+    document.querySelector('.message').textContent = '📉 Too low!';
+  }
+});
